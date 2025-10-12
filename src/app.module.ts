0fd8a11b4e23b1e5912172765c59aa5eb20b34f5
Salvador Bacci <salvador_bacci@hotmail.com>
2025-10-12 05:43:33 -0300

    first commit: added auth base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { auth } from './lib/auth';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule.forRoot({
+      auth,
+      disableGlobalAuthGuard: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { PrismaUsersRepository } from './repository/users.repository';
+
+@Module({
+  controllers: [UsersController],
+  providers: [
+    UsersService,
+    { provide: 'UsersRepository', useClass: PrismaUsersRepository },
+  ],
+})
+export class UsersModule { }
