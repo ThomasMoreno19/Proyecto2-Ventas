43beb3a36beb328d11f54681c2f6745a3d8c87de
carosuppo <carosu808@gmail.com>
2025-10-24 20:53:50 -0300

    refactor: remove MarcaXlinea DTOs, controller, service, and tests

--- src/marca-xlinea/dto/update-marca-xlinea.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMarcaXlineaDto } from './create-marca-xlinea.dto';
-
-export class UpdateMarcaXlineaDto extends PartialType(CreateMarcaXlineaDto) {}
