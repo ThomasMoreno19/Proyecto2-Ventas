54c61986e7c31d195b0f61bdda35e8769ff62ab8
unknown <giudicitomas1@gmail.com>
2025-10-26 02:04:15 -0300

    asd

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,36 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+
+  // Limita Jest al código fuente
+  roots: ['<rootDir>/src'],
+
   moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1', 
+    '^src/(.*)$': '<rootDir>/src/$1',
     '^@common/(.*)$': '<rootDir>/src/common/$1',
   },
+
   moduleFileExtensions: ['ts', 'js', 'json'],
+
   testMatch: ['**/*.spec.ts'],
+
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
+
+  // Solo recolecta cobertura del código fuente (no tests, ni dist)
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.spec.ts',
+    '!src/main.ts',
+  ],
+
   coverageDirectory: 'coverage',
+
+  // Ignora node_modules y dist tanto en tests como en cobertura
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+
   transformIgnorePatterns: [
     '/node_modules/(?!(jose|@thallesp/nestjs-better-auth|@noble/ciphers|@noble/hashes)/.*)',
   ],
