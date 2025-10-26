2223d4c5ff2fbb82e6a43f3bcb13014d535ff7e3
carosuppo <carosu808@gmail.com>
2025-10-26 03:03:46 -0300

    Cambios en jest.config.js, ahora lee bien los test

--- jest.config.js
+++ jest.config.js
@@ -6,15 +6,17 @@ module.exports = {
     '^@common/(.*)$': '<rootDir>/src/common/$1',
   },
   moduleFileExtensions: ['ts', 'js', 'json'],
-  testMatch: ['**/*.spec.ts'],
+  testMatch: '**/*.spec.ts',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
+  collectCoverageFrom: [
+  'src/**/*.ts',
+  '!src/main.ts',
+  '!src/**/*.module.ts',
+  ],
   coverageDirectory: 'coverage',
   transformIgnorePatterns: [
     '/node_modules/(?!(jose|@thallesp/nestjs-better-auth|@noble/ciphers|@noble/hashes)/.*)',
   ],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
